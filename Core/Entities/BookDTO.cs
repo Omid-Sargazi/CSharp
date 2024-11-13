e266d8ab0ec87af17cd7e01e1b76a5881e8fb45d
Omid-Sargazi <om.sargazi@gmail.com>
2024-11-13 23:26:13 +0330

    Core Layer BookDTO Entities

---
+++ Core/Entities/BookDTO.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibrarySystem.Core.Entities
+{
+    public class BookDTO
+    {
+        public string Title { get; set; }
+        public string ISBN { get; set;}
+    }
+}
