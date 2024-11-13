b673f25bea0f5a1abba5b8df6cad367957851670
Omid-Sargazi <om.sargazi@gmail.com>
2024-11-13 23:17:50 +0330

    initi

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LibrarySystems
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
