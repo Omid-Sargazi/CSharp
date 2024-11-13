04425595b885cbdcd12ff19c8f546d4ca815a565
Omid-Sargazi <om.sargazi@gmail.com>
2024-11-13 23:23:16 +0330

    Core Layer Book Entity

---
+++ Core/Entities/Book.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibrarySystem.Core.Entities
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ISBN { get; set; }
+        public int AuthorId { get; set; }
+        public Author Author { get; set; }
+    }
+}
